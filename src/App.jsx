7f9743e2359aa28f4191de820879a811cd6a74e7
Rendrez <rendrez0027@gmail.com>
2022-10-14 15:21:39 +0700

    menambah const style

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,28 @@
 import { Tab } from "@headlessui/react";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import About from "./component/About";
 import Contact from "./component/Contact";
 import Footer from "./component/Footer";
 import Home from "./component/Home";
-import Navbar from "./component/Navbar";
 import Work from "./component/Work";
 
 function App() {
+  const style = {
+    tab: "text-center px-2 ui-selected:bg-black ui-selected:rounded-xl ui-selected:text-white ui-not-selected:bg-white ui-not-selected:text-black",
+    tablist:
+      "flex justify-between border-b-2 bg-white py-2 text-lg font-medium",
+    brand: "text-3xl font-libre",
+    sticky: "sticky top-0 left-0 right-0",
+  };
   return (
     <Tab.Group>
-      <div className="sticky top-0 left-0 right-0">
-        <Tab.List
-          className={
-            "flex justify-between border-b-2 bg-white py-2 text-lg font-medium"
-          }
-        >
-          <h1>Randy Portofolio</h1>
+      <div className={style.sticky}>
+        <Tab.List className={style.tablist}>
+          <h1 className={style.brand}>Randy Portofolio</h1>
           <div>
-            <Tab className="text-center px-2 ui-selected:bg-black ui-selected:rounded-xl ui-selected:text-white ui-not-selected:bg-white ui-not-selected:text-black">
-              Home
-            </Tab>
-            <Tab className="text-center px-2 ui-selected:bg-black ui-selected:rounded-xl ui-selected:text-white ui-not-selected:bg-white ui-not-selected:text-black">
-              About
-            </Tab>
-            <Tab className="text-center px-2 ui-selected:bg-black ui-selected:rounded-xl ui-selected:text-white ui-not-selected:bg-white ui-not-selected:text-black">
-              Project
-            </Tab>
-            <Tab className="text-center px-2 ui-selected:bg-black ui-selected:rounded-xl ui-selected:text-white ui-not-selected:bg-white ui-not-selected:text-black">
-              Contact
-            </Tab>
+            <Tab className={style.tab}>Home</Tab>
+            <Tab className={style.tab}>About</Tab>
+            <Tab className={style.tab}>Project</Tab>
+            <Tab className={style.tab}>Contact</Tab>
           </div>
           {/* ...  */}
         </Tab.List>
--- src/component/About.jsx
+++ src/component/About.jsx
@@ -3,15 +3,18 @@ import { RiReactjsFill } from "react-icons/ri";
 import { SiBootstrap, SiTailwindcss } from "react-icons/si";
 
 export default function About() {
+  const style = {
+    itemJustifyCenter: "flex items-center justify-center",
+    skils: "border-2 border-black rounded-2xl px-2 flex items-center gap-2",
+    profilImg: "h-60 w-60 mx-auto rounded-[20px] overflow-hidden border-2",
+    description: "text-2xl mt-[40px] font-serif text-justify",
+  };
   return (
-    <section
-      id="About"
-      className="flex items-center justify-center min-h-[90vh]"
-    >
+    <section id="About" className={`${style.itemJustifyCenter} min-h-[90vh]`}>
       <div>
-        <div className="flex justify-between items-center gap-5">
+        <div className={`${style.itemJustifyCenter} gap-5`}>
           {/* <image */}
-          <div className="h-60 w-60 mx-auto rounded-[20px] overflow-hidden border-2">
+          <div className={style.profilImg}>
             <img src={profil} alt="profil" />
           </div>
           {/* image/> */}
@@ -19,7 +22,7 @@ export default function About() {
             <h1 className="text-4xl font-medium font-libre text-center">
               About Me
             </h1>
-            <p className="text-2xl mt-[40px] font-serif text-justify">
+            <p className={style.description}>
               I am Muhammad Randy a passionate front end web developer. I am a
               beginner in the front end web developer and still worked with a
               varied web developer framework like tailwind and react js.
@@ -32,17 +35,17 @@ export default function About() {
               My Skill
             </h1>
             <div className="flex justify-center mt-4 gap-4 text-xl font-medium">
-              <div className="border-2 border-black rounded-2xl px-2 flex items-center gap-2">
+              <div className={style.skils}>
                 <RiReactjsFill className="text-blue-500" />
                 <h4 className="font-libre">React Js Framework</h4>
               </div>
-              <div className="border-2 border-black rounded-2xl px-2 flex items-center gap-2">
+              <div className={style.skils}>
                 <SiTailwindcss className="text-blue-500" />
                 <h4 className="font-libre">Tailwind Framework</h4>
               </div>
-              <div className="border-2 border-black rounded-2xl px-2 flex items-center gap-2">
+              <div className={style.skils}>
                 <SiBootstrap className="text-purple-600" />
-                <h4 className="font-libre">React Js Framework</h4>
+                <h4 className="font-libre">Bootstrap Framework</h4>
               </div>
             </div>
           </div>
