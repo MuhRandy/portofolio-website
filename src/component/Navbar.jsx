1537434639fe78c843ca6358a8aa352690762f04
unknown <muhammadrandy0027@gmail.com>
2023-12-01 10:27:06 +0800

    update navbar bg blur

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,49 +1,56 @@
-import { IconSun, IconMoon, IconCode } from "@tabler/icons";
-import clsx from "clsx";
-import { motion } from "framer-motion";
-import { useState } from "react";
-import { Link } from "react-scroll";
+import { IconSun, IconMoon, IconCode } from '@tabler/icons';
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import { Link } from 'react-scroll';
 
 export default function Navbar() {
   const navs = [
     {
-      name: "Home",
-      link: "About",
+      name: 'Home',
+      link: 'About',
       offset: -50,
     },
     {
-      name: "Projects",
-      link: "Works",
+      name: 'Projects',
+      link: 'Works',
       offset: -50,
     },
     {
-      name: "Contact",
-      link: "Contact",
+      name: 'Contact',
+      link: 'Contact',
       offset: -50,
     },
   ];
   const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <nav className="sticky top-0 left-0 right-0 bg-white flex items-center justify-between font-inter z-20">
+    <nav
+      className={clsx(
+        'bg-white/10 font-inter w-screen h-14',
+        'fixed top-0 left-0 right-0 z-20',
+        'flex items-center justify-between',
+        'backdrop-blur-md'
+      )}
+    >
       <div
-        className={clsx("text-sm", "flex items-center gap-1", [
-          "sm:text-lg sm:font-bold",
-          "md:text-2xl",
+        className={clsx('text-sm', 'flex items-center gap-1', [
+          'sm:text-lg sm:font-bold',
+          'md:text-2xl',
         ])}
       >
         <IconCode />
         <span>Randy Portofolio's</span>
       </div>
       <div>
-        <ul className={clsx("text-md pr-5", "flex gap-2", "md:text-lg")}>
+        <ul className={clsx('text-md pr-5', 'flex gap-2', 'md:text-lg')}>
           {/* Toggle darkmode */}
           <li>
             <div
               className={clsx(
-                "flex justify-between",
-                "border-2 border-black rounded-2xl relative p-1 px-2 cursor-pointer",
-                "text-sm"
+                'flex justify-between',
+                'border-2 border-black rounded-2xl relative p-1 px-2 cursor-pointer',
+                'text-sm'
               )}
               onClick={() => setDarkMode(!darkMode)}
             >
@@ -51,8 +58,8 @@ export default function Navbar() {
               <IconSun size={15} />
               <div
                 className={clsx(
-                  "rounded-full absolute bg-black w-5 h-5 top-[1px]",
-                  darkMode ? "left-[2px]" : "right-[2px]"
+                  'rounded-full absolute bg-black w-5 h-5 top-[1px]',
+                  darkMode ? 'left-[2px]' : 'right-[2px]'
                 )}
               ></div>
             </div>
@@ -62,7 +69,7 @@ export default function Navbar() {
             <motion.div
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.9 }}
-              transition={{ duration: 0.5, type: "spring" }}
+              transition={{ duration: 0.5, type: 'spring' }}
               key={index}
             >
               <Link
